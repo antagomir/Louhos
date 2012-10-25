d8a571553026bbd4faf934bcf00df355196e7914
antagomir <lei@MIB12001>
2012-10-25 11:04:23 +0200

    get.branches

---
+++ build.R
@@ -0,0 +1,29 @@
+library(devtools)
+
+print("document")
+document("sorvi")
+
+#print("check")
+#check("sorvi")
+
+print("build")
+build("sorvi")
+
+print("install")
+install("sorvi")
+
+
+#print("Staticdocs")
+#library(staticdocs)
+#build_package("sorvi", "sorvi-staticdoc") 
+#system("cp -r sorvi-staticdoc/* homepage/sorvi.github.com/staticdocs/")
+
+#wd <- getwd()
+#setwd("homepage/sorvi.github.com/staticdocs/")
+#system("git pull")
+#system("git add *.html")
+#system("git commit -a -m'staticdocs update'")
+#system("git push")
+#setwd(wd)
+
+
