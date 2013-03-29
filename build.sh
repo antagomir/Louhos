a08f02dfef5b0b93e824a6db270bd0ca47f27a03
antagomir <antagomir@opencomp>
2013-03-29 12:48:58 +0100

    DVA loppurap

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ~/bin/R-2.15.2/bin/R CMD BATCH document.R
-~/bin/R-2.15.2/bin/R CMD check
-~/bin/R-2.15.2/bin/R CMD build
-~/bin/R-2.15.2/bin/R CMD INSTALL sorvi_0.2.11.tar.gz
+~/bin/R-2.15.2/bin/R CMD check sorvi
+~/bin/R-2.15.2/bin/R CMD build sorvi
+~/bin/R-2.15.2/bin/R CMD INSTALL sorvi_0.2.25.tar.gz
