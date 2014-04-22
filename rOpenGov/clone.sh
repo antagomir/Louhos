a85ff69989f3fff1888d8c737e1e0f3291b0edcb
antagomir <antagomir@lei>
2014-04-22 15:14:33 +0200

    ok

--- rOpenGov/clone.sh
+++ rOpenGov/clone.sh
@@ -1,4 +1,6 @@
 git clone git@github.com:rOpenGov/ropengov.github.io.git
 git clone git@github.com:rOpenGov/ropengov.github.io.wiki.git
 git clone git@github.com:rOpenGov/helsinki.git
- 
+git clone git@github.com:rOpenGov/sotkanet.git
+git clone git@github.com:rOpenGov/statfi.git 
+git clone git@github.com:rOpenGov/eurostat.git
