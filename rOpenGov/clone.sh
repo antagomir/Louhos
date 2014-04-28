b3efa65be884d13e08bca31d07015787251efce8
antagomir <leo.lahti@iki.fi>
2014-04-28 20:15:57 +0200

    sorvi

--- rOpenGov/clone.sh
+++ rOpenGov/clone.sh
@@ -4,3 +4,4 @@ git clone git@github.com:rOpenGov/helsinki.git
 git clone git@github.com:rOpenGov/sotkanet.git
 git clone git@github.com:rOpenGov/statfi.git 
 git clone git@github.com:rOpenGov/eurostat.git
+git clone git@github.com:rOpenGov/sorvi.git
